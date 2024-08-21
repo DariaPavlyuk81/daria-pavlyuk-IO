bb73d6ad2783475b2c943ad0abae103db4b71916
Daria Pavlyuk <daria.pavlyuk@testpro.io>
2024-08-21 19:40:19 -0400

    corrected js

--- js/index.js
+++ js/index.js
@@ -1,47 +1,45 @@
 //create a footer element
-const aFooter = document.createElement ("Footer");
-aFooter.className = "footer";
-aFooter.setAttribute("id","myfooter");
-document.body.appendChild(aFooter);
-console.log(aFooter);
+const footer = document.createElement ("footer");
+// footer.className = "footer";
+// footer.setAttribute("id","myfooter");
+document.body.appendChild(footer);
+
+const today = new Date();
 
 //get the current year
-const today = new Date();
-const thisYear = today.getFullYear()
-console.log(thisYear);
+
+const thisYear = today.getFullYear();
 
 //added footer element
 const newFooter = document.querySelector("footer");
-console.log(newFooter);
 
 //copyright
-const aCopyright = document.createElement("P");
+const copyright = document.createElement("p");
 const copyrightSymbol = "\u00A9";
-const copyrightText = copyrightSymbol + "DariaPavlyuk" + thisYear;
-aCopyright.innerHTML=copyrightText;
-console.log(aCopyright);
+const copyrightContent = `${copyrightSymbol} DariaPavlyuk ${thisYear};
+copyright.append(copyrightContent);
+footer.append(copyright);
 
-//append copyright to footer
-newFooter.appendChild(aCopyright);
-console.log(newFooter);
 
 // skills section
 const skills =["JavaScript", "HTML",  "CSS", "GitHUb"];
-console.log(skills);
+
 
 //sskills + DOM
-const skillsSection = document.querySelector("#skills-section");
-console.log(skillsSection);
-const skillsList = document.getElementById("skills-list");
-console.log(skillsList);
+const skillsSection = document.querySelector("#skills");
+
+const skillsList = skillsSection.getElementsByTagNAme("ul")[0];
+
+
 
 for (let i = 0; i< skills.length; i++)
 {
     const skill = document.createElement("li");
+
     skill.innerText = skills[i];
-    console.log(skill);
+    
     skillsList.appendChild(skill);
-    console.log(skillsList);
+    
 }
 //add new skill
 const skillButton = document.getElementById("skill-button");
