163d6a56ddf317d2b28833f1931deb08eebd3169
Daria Pavlyuk <pavlyuk.daria81@gmail.com>
2024-09-23 22:33:35 -0400

    message-section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //create a footer element
-const footer = document.createElement ("footer");
+const footer = document.createElement("footer");
 
 document.body.appendChild(footer);
 
@@ -19,142 +19,129 @@ const copyrightContent = `${copyrightSymbol} DariaPavlyuk ${thisYear}`;
 copyright.append(copyrightContent);
 footer.append(copyright);
 
-
 // skills section
-const skills =["JavaScript", "HTML",  "CSS", "GitHub"];
-
+const skills = ["JavaScript", "HTML", "CSS", "GitHub"];
 
 //skills + DOM
 const skillsSection = document.querySelector("#skills-section");
 
 const skillsList = skillsSection.getElementsByTagName("ul")[0];
 
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("LI");
 
+  skill.innerText = skills[i];
 
-for (let i = 0; i< skills.length; i++)
-{
-    const skill = document.createElement("LI");
-
-    skill.innerText = skills[i];
-    
-    skillsList.appendChild(skill);
-    
+  skillsList.appendChild(skill);
 }
+
+const messageSection = document.getElementById("messages");
+messageSection.hidden = true;
 //find the leave messages form and add the callback for submit
 const messageForms = document.getElementsByName("leave_message");
-const messageForm= messageForms[0];
+const messageForm = messageForms[0];
 
 //Attach the listener
 messageForm.addEventListener("submit", onFormSubmit);
 
 //callback for submit
-function onFormSubmit(event){
-    event.preventDefault();
+function onFormSubmit(event) {
+  event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const userName = formData.get("usersName");
-    const email = formData.get("usersEmail");
-    const userMessage = formData.get("usersMessage");
-    
+  const formData = new FormData(event.target);
+  const userName = formData.get("usersName");
+  const email = formData.get("usersEmail");
+  const userMessage = formData.get("usersMessage");
 
-    const messageSection = document.getElementById("messages");
-    //messageSection.style.display = "block";
-    messageSection.hidden = false;
+  //const messageSection = document.getElementById("messages");
+  //messageSection.style.display = "block";
+  messageSection.hidden = false;
 
-//display message beneath messages section
-const messageList = messageSection.querySelector("ul");
+  //display message beneath messages section
+  const messageList = messageSection.querySelector("ul");
 
-
-//display the message section
-const newMessage = document.createElement("li");
-newMessage.classList.add("message-list");
-const newMessageContent = `<a href="mailto:${email}">${userName}</a>
+  //display the message section
+  const newMessage = document.createElement("li");
+  newMessage.classList.add("message-list");
+  const newMessageContent = `<a href="mailto:${email}">${userName}</a>
 <span>${userMessage}</span>`;
-newMessage.innerHTML = newMessageContent;
+  newMessage.innerHTML = newMessageContent;
 
+  //Add a remove button
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.type = "button";
+  removeButton.classList.add("remove-button");
+  removeButton.name = "remove-button";
 
-//Add a remove button
-const removeButton = document.createElement("button");
-removeButton.textContent = "Remove";
-removeButton.type ="button";
-removeButton.classList.add("remove-button");
-removeButton.name = "remove-button";
-
-
-//add event listener
-removeButton.addEventListener("click",(event)=>{
-    const entry = event.target.parentNode;
-    entry.remove();
-
-    const messageSection = document.getElementById ("messages");
-    const messageList = messageSection.getElementsByTagName("ul")[0];
-    if (messageList.children.length=== 0){
-        //messageSection.hidden="none";
-        messageSection.hidden = true;
-    }
-});
-
-
-//Add remove Button
-newMessage.appendChild(removeButton);
-//add message
-messageList.appendChild(newMessage);
-
-//reset the form
-event.target.reset();
-}
-
-
-//callback for remove button
-function onRemoveButton(event){
+  //add event listener
+  removeButton.addEventListener("click", (event) => {
     const entry = event.target.parentNode;
     entry.remove();
 
     const messageSection = document.getElementById("messages");
-    const messageList = messageSection.getElementsByTagName("li");
-    if (messageList.length === 0){
-        messageSection.style.display = "none";
+    const messageList = messageSection.getElementsByTagName("ul")[0];
+    if (messageList.children.length === 0) {
+      //messageSection.hidden="none";
+      messageSection.hidden = true;
     }
+  });
+
+  //Add remove Button
+  newMessage.appendChild(removeButton);
+  //add message
+  messageList.appendChild(newMessage);
+
+  //reset the form
+  event.target.reset();
 }
+
+// //callback for remove button
+// function onRemoveButton(event){
+//     const entry = event.target.parentNode;
+//     entry.remove();
+
+//     const messageSection = document.getElementById("messages");
+//     const messageList = messageSection.getElementsByTagName("li");
+//     if (messageList.length === 0){
+//         messageSection.style.display = "none";
+//     }
+// }
+
 // get the repositories from github
 const userName = "DariaPavlyuk81";
 fetch(`https://api.github.com/users/${userName}/repos`)
-
-//get the response
-.then((response) => {
-    if(!response.ok){
-        throw new Error("Request failed");
+  //get the response
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Request failed");
     }
     return response.json();
-})
+  })
 
-//get the data and add it to the html
-.then((data) => {
+  //get the data and add it to the html
+  .then((data) => {
     console.log("JSON  data:", data);
     const repositories = [...data];
-    console.log("Repositories array:",repositories);
+    console.log("Repositories array:", repositories);
 
+    //find the project section
+    const projectSection = document.getElementById("projects-section");
+    const projectList = projectSection.getElementsByTagName("UL");
 
-//find the project section
-const projectSection = document.getElementById("projects-section");
-const projectList = projectSection.getElementsByTagName("UL");
+    //create the repostories list by adding to the html
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("LI");
+      const repoUrl = repositories[i].html_url;
 
+      const projectLink = document.createElement("a");
+      projectLink.href = repoUrl;
+      projectLink.textContent = repoUrl;
+      project.appendChild(projectLink);
 
-
-//create the repostories list by adding to the html
- for (let i=0; i < repositories.length; i++){
-    const project = document.createElement("LI");
-    const repoUrl = repositories[i].html_url;
-
-    const projectLink = document.createElement("a");
-    projectLink.href = repoUrl;
-    projectLink.textContent = repoUrl;
-    project.appendChild(projectLink);
-    
-     projectList[0].appendChild(project);
- }
-})
-.catch((error) => {
-    console.error("An error occurred:",error);
-
- });
+      projectList[0].appendChild(project);
+    }
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
